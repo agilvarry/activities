119db614f7d093236edbc70f6728b6ec4c5d8cbf
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-07-06 12:18:16 -0600

    React CRUD interface

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <App />
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
