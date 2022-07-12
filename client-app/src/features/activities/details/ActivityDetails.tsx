46701f699603a3d87844cd3f093ed56529e14b01
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-07-12 12:38:09 -0600

    React Router v5

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import { Button, Card, Image } from "semantic-ui-react";
 import LoadingComponents from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/store";
 
 
 
-export default function ActivityDetails() {
-    const {activityStore} = useStore();
-    const{selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+export default observer(function ActivityDetails() {
+    const { activityStore } = useStore();
+    const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
+    const { id } = useParams<{ id: string }>();
 
-    if (!activity) return <LoadingComponents />;
+    useEffect(() => {
+        if (id) loadActivity(id);
+    }, [id, loadActivity]);
+
+    if (loadingInitial || !activity) return <LoadingComponents />;
 
     return (
         <Card fluid>
-            <Image  src={`/assets/categoryImages/${activity.category}.jpg`}/>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
             <Card.Content>
                 <Card.Header>{activity.title}</Card.Header>
                 <Card.Meta><span>{activity.date}</span> </Card.Meta>
@@ -23,11 +30,11 @@ export default function ActivityDetails() {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
+                    <Button as={Link} to='/activities' basic color='grey' content='Cancel' />
                 </Button.Group>
 
             </Card.Content>
         </Card>
     )
-}
+});
