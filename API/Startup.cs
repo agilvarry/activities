75dd53f334b1888c7524ea121c832ce58cab4932
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-07-15 12:17:40 -0600

    JWT Authentication

--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,8 @@ using Application.Activities;
 using Application.Core;
 using FluentValidation.AspNetCore;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Persistence;
@@ -24,12 +26,19 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers().AddFluentValidation(config =>
+            //everything requires authentication
+            services.AddControllers(opt => {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
+            
+            .AddFluentValidation(config =>
             {
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(_config);
+
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +57,7 @@ namespace API
 
             app.UseCors("CorsPolicy");
 
+            app.UseAuthentication(); //must go before authorization
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
