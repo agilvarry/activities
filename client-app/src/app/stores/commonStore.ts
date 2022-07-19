682db31b8ebd54fd58521c6e167e1abb15774dc6
Alex Gilvarry <31046803+agilvarry@users.noreply.github.com>
2022-07-19 14:59:25 -0600

    client side login

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,13 +1,35 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ServerError } from "../models/ServerError";
 
 export default class CommonStore {
-    error: ServerError | null = null;
-    constructor(){
-        makeAutoObservable(this);
-    }
+  error: ServerError | null = null;
+  token: string | null = window.localStorage.getItem("jwt");
+  appLoaded = false;
 
-    setServerError = (error: ServerError) => {
-        this.error = error;
-    }
+  constructor() {
+    makeAutoObservable(this);
+
+    reaction(
+      () => this.token,
+      (token) => {
+        if (token) {
+          window.localStorage.setItem("jwt", token);
+        } else {
+          window.localStorage.removeItem("jwt");
+        }
+      }
+    );
+  }
+
+  setServerError = (error: ServerError) => {
+    this.error = error;
+  };
+
+  setToken = (token: string | null) => {
+    this.token = token;
+  };
+
+  setAppLoaded = () => {
+    this.appLoaded = true;
+  };
 }
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,21 +1,40 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
-export default function HomePage() {
-    return (
-      <Segment inverted textAlign='center' vertical className='masthead'>
-        <Container text>
+export default observer(function HomePage() {
+  const { userStore, modalStore } = useStore();
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
         <Header as='h1' inverted>
-        <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
-        Activities
+          <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
+          Activities
         </Header>
-        <Header as='h2' inverted content='Welcome to Activities' />
-        <Button as={Link} to='/activities' size='huge' inverted>
-            Take me there!
-        </Button>
-        </Container>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as='h2' inverted content='Welcome to Activities' />
+            <Button as={Link} to='/activities' size='huge' inverted>
+              go to activities!
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button onClick={() => modalStore.openModal(<LoginForm />)} size='huge' inverted>
+              Log Me In!
+            </Button>
+            <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
+              Register
+            </Button>
+          </>
 
-      </Segment>
-    )
-}
+        )}
+      </Container>
+
+    </Segment>
+  )
+});
